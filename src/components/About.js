f9e203485a1232322407e62e9b911f9d04793562
Candice Smith MacKenzie <Mahony8@aol.com>
2022-02-07 18:12:55 -0500

    completed asignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
-  return (
+function About(props) { 
+  let bioline
+  if(!props.bio){
+    bioline = null
+  } else { 
+    bioline = <p>{props.bio}</p>
+}
+    return(
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioline}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links links={props.links} linkedin={props.links.linkedin} github={props.links.github}
+      /> 
     </div>
   );
 }
